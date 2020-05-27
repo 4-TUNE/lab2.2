db7124d54d04565775f130b5648e0f50333ce5ea
4-TUNE <61144719+4-TUNE@users.noreply.github.com>
2020-05-27 14:55:14 +0300

    Добавьте файлы проекта.

---
+++ lab2.2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace oopLaba2_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("|     x     |    y=f(x)   |");
+            double a = 1.25, b = 6.75;
+            double y;
+            double x = a;
+            while (b > x)
+            {
+                y = Math.Pow(x, 1.0/4.0);
+                x = x + 0.25;
+                Console.WriteLine(x + "\t    " + y);
+            }
+        }
+    }
+}
